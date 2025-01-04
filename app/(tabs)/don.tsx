d7f3a6535055f6077f87ee2828b34c11e1686d6c
antoine <antoine_arranz@hotmail.com>
2025-01-04 20:28:24 +0100

    init project

---
+++ app/(tabs)/don.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function DonScreen() {
+  return (
+    <View>
+      <Text>Don Screen</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/signalement.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SignalementScreen() {
+  return (
+    <View>
+      <Text>Signalement Screen</Text>
+    </View>
+  );
+}
